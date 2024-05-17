7091fb2313f38114076befbb942b0b4f5928383a
mtardunix <marius.tuschl@wwv.de>
2024-05-17 14:20:20 +0200

    fixed old libs

--- unified_test.py
+++ unified_test.py
@@ -1,4 +1,4 @@
-from time import clock
+from time import process_time
 from acktr.model_loader import nnModel
 from acktr.reorder import ReorderTree
 import gym
@@ -11,7 +11,7 @@ def run_sequence(nmodel, raw_env, preview_num, c_bound):
     obs = env.cur_observation
     default_counter = 0
     box_counter = 0
-    start = clock()
+    start = process_time()
     while True:
         box_list = env.box_creator.preview(preview_num)
         # print(box_list)
@@ -19,7 +19,7 @@ def run_sequence(nmodel, raw_env, preview_num, c_bound):
         act, val, default = tree.reorder_search()
         obs, _, done, info = env.step([act])
         if done:
-            end = clock()
+            end = process_time()
             print('Time cost:', end-start)
             print('Ratio:', info['ratio'])
             return info['ratio'], info['counter'], end-start,default_counter/box_counter
@@ -80,4 +80,4 @@ if __name__ == '__main__':
     # args.enable_rotation = True
     # unified_test('pretrained_models/rotation_cut_2.pt', args, pruning_threshold)
 
-    
+
